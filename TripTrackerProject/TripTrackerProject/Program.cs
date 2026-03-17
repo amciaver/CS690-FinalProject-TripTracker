e33b0cd32f5f0395285d9019beba23c512fb5aaa
Anthony Ciaverella <anthonyciaverella@gmail.com>
2026-03-17 16:44:29 +0000

    domain refactoring

--- TripTrackerProject/TripTrackerProject/Program.cs
+++ TripTrackerProject/TripTrackerProject/Program.cs
@@ -1,72 +1,130 @@
 ﻿namespace TripTrackerProject;
 
+using Microsoft.VisualBasic;
 using Spectre.Console;
 using System.IO;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Welcome to the Trip Tracker Application!");
-
-        List<string> tripList = new List<string>();
-        var trips = File.ReadAllLines("trips.txt");
-        foreach (var trip in trips){
-            tripList.Add(trip);
+    {   
+        Console.WriteLine(Environment.NewLine + "Welcome to the TripTracker Application!");
+        List<Trip> Trips = new List<Trip>();
+        
+        var tripsFileContent = File.ReadAllLines("trips.txt");
+        foreach(var tripName in tripsFileContent){
+            Trips.Add(new Trip(tripName));
         }
-        tripList.Add("Enter New Trip");
-        tripList.Add("Exit Application");
+
+        foreach(Trip trip in Trips){
+            string fileName = trip.Name + ".txt";
+            if (File.Exists(fileName))
+            {
+                var fileData = File.ReadAllLines(fileName);
+                foreach(var line in fileData)
+                {
+                    var splitted = line.Split(",",StringSplitOptions.RemoveEmptyEntries);
+                    var photoName = splitted[1];
+                    var photoLocation = splitted[2];
+                    var photoTimeOfDay = splitted[3];
+                    Photo readPhoto = new Photo(photoName,photoLocation,photoTimeOfDay);
+                    trip.Photos.Add(readPhoto);
+                }
+            }
+        }
+        List<string> tripSelectChoices = Trips.ConvertAll(t => t.Name);
+        tripSelectChoices.Add("Enter New Trip");
+        tripSelectChoices.Add("Exit Application");
         
         string selectedTrip = null;
-        do{
-            selectedTrip = AnsiConsole.Prompt(
-            new SelectionPrompt<string>()
-                .Title("Please select a Trip:")
-                .AddChoices(tripList));
+        do{    
+            selectedTrip = AskForSelection("Please select a Trip", tripSelectChoices);
             Console.WriteLine("You have selected: " + selectedTrip);
-
+        
+            string tripListFileName = "trips.txt";
+            
             string tripName = null;
             if(selectedTrip == "Enter New Trip"){
-                Console.WriteLine("Please Enter The Trip Name: ");
-                tripName = Console.ReadLine();
-                if(tripList.Contains(tripName)){
+                tripName = AskForInput("Enter new trip name: ");
+                if(tripSelectChoices.Contains(tripName)){
                     Console.WriteLine("Trip already exists!");
                 }else{
-                File.AppendAllText("trips.txt", tripName + Environment.NewLine);
-                tripList.Add(tripName);
-                }
+                tripSelectChoices.Add(tripName);
+                Trip newTripName = new Trip(tripName);
+                Trips.Add(newTripName);
+                SyncTrips(Trips);
                 selectedTrip = tripName;
+                }
             }
+
             string trackEntryCommand = null;
             if(selectedTrip != "Exit Application"){
                 do{
                     Console.WriteLine(Environment.NewLine + "Selected Trip = " + selectedTrip);
-                    trackEntryCommand = AnsiConsole.Prompt(
-                        new SelectionPrompt<string>()
-                        .Title("Please select an action")
-                        .AddChoices("Track Photo", "Exit"));
                     
+                    List<string> trackEntryCommandChoices = new List <string> {"Track Photo", "Exit"};
+                    trackEntryCommand = AskForSelection("Please select an action", trackEntryCommandChoices);
                     Console.WriteLine("Selected Action = " + trackEntryCommand);
                     
                     if(trackEntryCommand == "Track Photo"){
-                        Console.WriteLine("Please enter the photo name:");
-                        string photoName = Console.ReadLine();
 
-                        Console.WriteLine(Environment.NewLine + "Please enter the photo location:");
-                        string photoLocation = Console.ReadLine();
-
-                        var photoTime = AnsiConsole.Prompt(
-                            new SelectionPrompt<string>()
-                            .Title("Please choose the time of day the photo was taken:")
-                            .AddChoices("Morning", "Day", "Night"));
+                        string photoName = AskForInput("Please enter the photo name:");
+                        string photoLocation = AskForInput(Environment.NewLine + "Please enter the photo location:");
+                        List<string> timeOfDayChoices = new List<string> {"Morning", "Day", "Night"};
+                        string photoTime = AskForSelection("Please select the time of day the photo was taken:", timeOfDayChoices);
                         
-                        string tripFileName = selectedTrip + ".txt";
-                        File.AppendAllText(tripFileName, photoName + "," + photoLocation + "," + photoTime + Environment.NewLine);
-                        Console.WriteLine(Environment.NewLine + "Photo information written to " + tripFileName + ": " +
-                        photoName + "," + photoLocation + "," + photoTime);
+                        Photo newPhoto = new Photo(photoName, photoLocation, photoTime);
+                        foreach(Trip trip in Trips)
+                        {
+                            if(selectedTrip == trip.Name)
+                            {
+                                trip.Photos.Add(newPhoto);
+                                string tripFileName = selectedTrip + ".txt";
+                                SyncPhotos(selectedTrip,Trips);
+                            }
+                        }
                     }
                 }while (trackEntryCommand!= "Exit");
             }
         }while(selectedTrip != "Exit Application");
+    }
+
+    public static string AskForInput(string message){
+        Console.WriteLine(message);
+        return Console.ReadLine();
+    }
+
+    public static string AskForSelection(string message, List<string> choices){
+        return AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title(message)
+            .AddChoices(choices));
+    }
+
+    public static void FileSaver(string fileName, string writeToFile){
+        File.AppendAllText(fileName, writeToFile);
+    }
+    public static void SyncTrips(List<Trip>Trips)
+    {
+        File.Delete("trips.txt");
+        foreach(Trip enteredTrip in Trips)
+        {
+            File.AppendAllText("trips.txt", enteredTrip + Environment.NewLine);
+        }
+        Console.WriteLine("Trips have been synchronized to the text file");
+    }
+
+    public static void SyncPhotos(string targetTrip, List<Trip>Trips)
+    {
+        foreach (Trip trip in Trips){
+            if (trip.Name == targetTrip){
+                string fileName = trip.Name + ".txt";
+                File.Delete(fileName);
+                foreach (Photo photo in trip.Photos){
+                    File.AppendAllText(fileName,"Photo," + photo.Name + "," + photo.Location + "," + photo.TimeOfDay + Environment.NewLine);
+                }
+                Console.WriteLine(Environment.NewLine + "Photos Synced for: " + trip.Name);
+            }
+        }
     }
 }
