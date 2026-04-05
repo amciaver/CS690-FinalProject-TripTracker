e1487c6d6bbb503955033687c0c86d16622cad53
Anthony Ciaverella <anthonyciaverella@gmail.com>
2026-04-05 16:47:51 +0000

    separated console UI into its own file

--- TripTrackerProject/TripTrackerProject/Program.cs
+++ TripTrackerProject/TripTrackerProject/Program.cs
@@ -6,74 +6,10 @@ using System.IO;
 
 class Program
 {
-    static void Main(string[] args)
-    {   
+    static void Main(string[] args){   
         Console.WriteLine(Environment.NewLine + "Welcome to the TripTracker Application!");
-        Datamanager datamanager = new Datamanager();
-        Reporter reporter = new Reporter();
-
-        List<string> tripSelectChoices = datamanager.Trips.ConvertAll(t => t.Name);
-        tripSelectChoices.Add("Enter New Trip");
-        tripSelectChoices.Add("Exit Application");
         
-        string selectedTrip;
-        do{    
-            selectedTrip = Datamanager.AskForSelection("Please select a Trip", tripSelectChoices);
-            Console.WriteLine("You have selected: " + selectedTrip);
-            
-            string tripName;
-            if(selectedTrip == "Enter New Trip"){
-                tripName = Datamanager.AskForInput("Enter new trip name: ");
-                if(tripSelectChoices.Contains(tripName)){
-                    Console.WriteLine("Trip already exists!");
-                }else{
-                tripSelectChoices.Add(tripName);
-                tripSelectChoices.Remove("Enter New Trip");
-                tripSelectChoices.Remove("Exit Application");
-                tripSelectChoices.Add("Enter New Trip");
-                tripSelectChoices.Add("Exit Application");
-
-                Trip newTripName = new Trip(tripName);
-                datamanager.Trips.Add(newTripName);
-                datamanager.SyncTrips(datamanager.Trips);
-                selectedTrip = tripName;
-                }
-            }
-
-            string trackEntryCommand;
-            if(selectedTrip != "Exit Application"){
-                do{
-                    Console.WriteLine(Environment.NewLine + "Selected Trip = " + selectedTrip);
-                    
-                    List<string> trackEntryCommandChoices = new List <string> {"Track Photo", "Track Cost","Track Note", "Display Trip Records", "Total Trip Cost", "Return To Home Menu"};
-                    trackEntryCommand = Datamanager.AskForSelection("Please select an action:", trackEntryCommandChoices);
-                    Console.WriteLine(Environment.NewLine + "Selected Action = " + trackEntryCommand);
-                    
-                    if(trackEntryCommand == "Track Photo"){
-
-                        Datamanager.TrackPhoto(datamanager, selectedTrip);
-
-                    }else if(trackEntryCommand == "Track Cost"){
-
-                        Datamanager.TrackCost(datamanager, selectedTrip);
-                    
-                    }else if(trackEntryCommand == "Track Note"){
-                        
-                        Datamanager.TrackNote(datamanager, selectedTrip);
-    
-                    }else if(trackEntryCommand == "Display Trip Records"){
-
-                        reporter.DisplayRecords(datamanager, selectedTrip);
-                        
-                    }else if(trackEntryCommand == "Total Trip Cost"){
-                       
-                        reporter.TotalTripCost(datamanager, selectedTrip);
-
-                    }
-                }while (trackEntryCommand!= "Return To Home Menu");
-            }
-        }while(selectedTrip != "Exit Application");
-    }
-
-    
+        ConsoleUI theUI = new ConsoleUI();
+        theUI.Show();    
+    }    
 }
