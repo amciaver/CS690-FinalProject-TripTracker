86a882d7a36110e3e020e9b7a00e0b676d91ca01
Anthony Ciaverella <anthonyciaverella@gmail.com>
2026-04-10 23:45:22 +0000

    separated asking for input functions into console UI

--- TripTrackerProject/TripTrackerProject.Test/ConsoleUITests.cs
+++ TripTrackerProject/TripTrackerProject.Test/ConsoleUITests.cs
@@ -6,16 +6,17 @@ using TripTrackerProject;
 public class ConsoleUITests{
 
     ConsoleUI testUI = new ConsoleUI();
-    Datamanager testManager = new Datamanager();
 
     [Fact]
     public void Ask_For_Selection_Test(){
-        var testManager = new Datamanager();
+        
         var testConsole = new TestConsole();
         testConsole.Interactive();
-        testManager.iAnsi = testConsole; 
+
+        testUI.consoleAnsi = testConsole; 
         testConsole.Input.PushKey(ConsoleKey.Enter);
-        var result = testManager.AskForSelection("Please Select a trip", new List<string> { "Florida 2025" });
+
+        var result = testUI.AskForSelection("Please Select a trip", new List<string> { "Florida 2025" });
         Assert.Equal("Florida 2025", result);
     }
 }
